ff2a476b85d5c23040eecdedb70b3aadb17f19a1
Abinet Bushura Cholo <157323021+Abinet-Cholo@users.noreply.github.com>
2025-04-02 04:09:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("fade-in-visible");
-        observer.unobserve(entry.target);
-      }
-    });
-  }, { threshold: 0.1 });
-
-  // Observe all elements with the class "fade-in"
-  document.querySelectorAll('.fade-in').forEach(element => {
-    observer.observe(element);
+// Animate motion for plane (Education section)
+document.addEventListener("DOMContentLoaded", () => {
+    const plane = document.querySelector("animateMotion");
+    if (plane) {
+      plane.setAttribute("dur", "10s");
+      plane.setAttribute("repeatCount", "indefinite");
+    }
   });
-});
+  
